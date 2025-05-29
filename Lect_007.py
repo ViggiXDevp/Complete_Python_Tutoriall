616c091353a418afa2522872401fc383fb70d4de
G V VIGHNESH REDDY <gvvighneshreddy8612@gmail.com>
2025-05-29 10:12:04 +0530

    if - else statements

--- Lect_007.py
+++ Lect_007.py
@@ -1,32 +1,19 @@
-# if-else & Elif use krte hai.
+# ======== if, elif and else statements ========
 
 num1 = int(input("Enter your 1st Number : "))
 num2 = int(input("Enter your 2nd Number : "))
 num3 = int(input("Enter your 3rd Number : "))
-if num1>num2:
-    print("number 1 is Greater than 2.")
-elif num1<num2:
-    print("number 1 is lesser than 2.")
-elif num1==num2:
-    print("Equal")    
-elif num1>num3:
-    print("number 1 is Greater than 3.")    
-elif num1<num3:
-    print("number 1 is lesser than 3.")   
-elif num1==num3:
-    print("number 1 is equal to number 3")     
-elif num2>num3:
-    print("number 2 is Greater than 3.")    
-elif num2<num3:
-    print("number 2 is lesser than 3.")   
-elif num2==num3:
-    print("number 2 is equal to number 3")        
+if (num1 > num2 and num1 > num3):
+    print("num1 is greater")
+elif(num2 > num1 and num2 > num3):
+    print("num2 is greater")
+elif(num3 > num1 and num3 > num2):
+    print("num3 is greater")
 else:
       print("Your input is wrong")
 
 
-
-'''A Programm for checking NUmber is in list or not '''
+''' Checking whether the number is present in the list '''
 
 list1 = [5, 7, 3]
 print(5 in list1)
@@ -34,12 +21,12 @@ if 5 in list1:
   print("Yes, it is in the list.")
 
 
-''' A programm For Eligibility for driving '''
+''' Checking the eligibility criteria for driving license '''
 
-age1 = int(input("Enter your Age : "))
-if age1>18:
-    print("Eligible for driving.")
-elif age1==18:
-    print("Please came back after sometime.")
+age = int(input("Enter your age : "))
+if age >= 18:
+    print("Eligible for permanent driving license.")
+elif age >= 16:
+    print("Eligible for learner's license (for non-geared vehicles).")
 else:
-    print("Not Eligible for driving.")
+    print("Not eligible for any driving license.")
