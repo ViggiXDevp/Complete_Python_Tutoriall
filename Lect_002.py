62e72ebbf6499369b2f09c5f4adc029d8b4f3640
G V VIGHNESH REDDY <gvvighneshreddy8612@gmail.com>
2025-06-17 21:36:28 +0530

    Data Types | Type Casting

--- Lect_002.py
+++ Lect_002.py
@@ -24,6 +24,6 @@ print(var2 + var3)    # This will print 10.14
 print(var4 + str(var5)) # Another type casting example
 
 
-print(10 * "Hello World !!!\n")             # Iske through hello world 10 times print hoga
-print(100 * (int(var5) + int(var2)))        # int(var5) + int(var2) iska value 14 aega ab usko bhi ham multiply kr sakte hai. ans is 1400
-print(100 * str(int(var5) + int(var2)))     # add hone baad wo 14 bana jisko ham str() iska matlab ab maschine 14 ko 100 times likh dega
+print(10 * "Hello World !!!\n")             # It will print Hello World !!! for 10 times
+print(100 * (int(var5) + int(var2)))        # int(var5) + int(var2) = 70. Now, we will multiply that value with 100
+print(100 * str(int(var5) + int(var2)))     # int(var5) + int(var2) = 70 is converted to a string. Now the string "70" will get printed for 100 times
